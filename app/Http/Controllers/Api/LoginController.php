ec7e0d74f9950ffba55bc897db953de32ed0c446
Ahmad <nayminzaw.dev@gmail.com>
2025-07-14 22:22:32 +0800

    Api auth flow finish

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -3,25 +3,56 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use function Sodium\add;
 
 class LoginController extends Controller
 {
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $request->validate([
             'email' => 'required|email',
             'password' => 'required|string|min:8'
         ]);
 
-        if(auth()->attempt($request->only('email', 'password'))){
+        if (auth()->attempt($request->only('email', 'password'))) {
             $user = auth()->user();
 
             $token = $user->createToken('auth_token')->plainTextToken;
 
             return response()->json(['user' => $user, 'token' => $token]);
         }
-        return response()->json(['message' => 'Invalid credentials!'],401);
+        return response()->json(['message' => 'Invalid credentials!'], 401);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+        $user = User::create($request->all());
+        if ($user) {
+            $user->assignRole('customer');
+            auth()->login($user);
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return response()->json(['user' => $user, 'token' => $token], 201);
+        }
+        return response()->json(['message' => 'Something went wrong'], 500);
+
+    }
+
+    public function getUser(Request $request)
+    {
+        return $request->user();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Auth routes
 Route::post('/login', [App\Http\Controllers\Api\LoginController::class, 'login']);
+Route::post('/register', [\App\Http\Controllers\Api\LoginController::class, 'register']);
+Route::post('/logout', [\App\Http\Controllers\Api\LoginController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::get('/user', [\App\Http\Controllers\Api\LoginController::class,'getUser'])->middleware('auth:sanctum');
+
